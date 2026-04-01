21a89f61fd4ddde9c4a8d23c513b7d8b8f505de0
Harsha Nandiwada <harsha@trackstarhq.com>
2026-04-01 14:49:12 -0700

    changed to configs

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -13,7 +13,7 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     config = function()
       require("nvim-treesitter.install").prefer_git = true
-      require("nvim-treesitter.config").setup({
+      require("nvim-treesitter.configs").setup({
         ensure_installed = { "python", "lua", "json", "bash", "markdown", "javascript", "typescript", "tsx" },
         highlight = { enable = true },
         indent = { enable = true },
